237934d49bab104e86f0949860dedea99275f7b6
Garnistarr <97260732+Garnistarr@users.noreply.github.com>
2025-10-01 01:34:57 +0200

    Update 2_📈_Manager_Dashboard.py

--- pages/2_📈_Manager_Dashboard.py
+++ pages/2_📈_Manager_Dashboard.py
@@ -1,27 +1,15 @@
 import streamlit as st
 
-# Set the title of the page
 st.title("📈 Manager Dashboard")
 
-# Check if the user is logged in, which is set in app.py
 if st.session_state.get('authentication_status'):
-    
-    # Get the user's role from the shared session state
-    user_role = st.session_state.get('user_data', {}).get("role")
+    user_role = st.session_state.user_data.get("role")
 
-    # Display content based on the user's role
     if user_role == "Manager":
         st.info("This is a placeholder for the Manager Dashboard. This feature is coming soon.")
-        st.write("Here, you will be able to see charts and metrics based on the data collected by Process Controllers.")
-    
-    elif user_role == "Process Controller":
-        st.warning("As a Process Controller, you do not have access to the Manager Dashboard.")
-    
+        st.write("Here, you will be able to see charts and metrics.")
     else:
-        st.error("Your role is not defined. Please contact an administrator.")
-
+        st.warning("You do not have access to the Manager Dashboard.")
 else:
-    # If the user is not logged in, show a warning message
     st.warning("Please log in on the Home page to access this page.")
-
 
