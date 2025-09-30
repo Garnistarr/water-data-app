49b5a04b1d29022bcf2d4059b45aa74e4b62f60e
Garnistarr <97260732+Garnistarr@users.noreply.github.com>
2025-10-01 01:41:40 +0200

    Update 1_💧_Water_Quality.py

--- pages/1_💧_Water_Quality.py
+++ pages/1_💧_Water_Quality.py
@@ -1,14 +1,6 @@
 import streamlit as st
 
-st.title("Test: Water Quality Page")
-
-st.success("If you can see this message, the page file is being recognized by Streamlit.")
-
-st.write("This is a temporary test to diagnose the sidebar issue.")
-
-# We can check the session state here to see what this page sees
-st.write("Current authentication status on this page:")
-st.write(st.session_state.get('authentication_status'))
-
+st.title("This is the Main Home Page")
+st.success("If you can see the sidebar on the left with a 'Test Page', then the multi-page structure is working.")
 
 
